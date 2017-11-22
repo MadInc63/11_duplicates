49be7ea9a4b471bf937f9461c8a7dc8fe09c4991
MadInc63 <MadInc63@gmail.com>
2017-11-22 09:54:09 +0300

    fix

--- duplicates.py
+++ duplicates.py
@@ -12,20 +12,20 @@ def parser_arg():
 
 
 def find_duplicate(folder):
-    duplicates = {}
+    list_of_files = {}
     for dirs, subdirs, files in os.walk(folder):
         for name in files:
             file_path = os.path.join(dirs, name)
             file_hash = hashlib.sha1(open(file_path, 'rb').read()).digest()
-            double = duplicates.get(file_hash)
-            if double:
+            add_duplicate_to_list = list_of_files.get(file_hash)
+            if add_duplicate_to_list:
                 try:
-                    duplicates[file_hash][name].append(file_path)
+                    list_of_files[file_hash][name].append(file_path)
                 except KeyError:
-                    duplicates[file_hash][name] = [file_path]
+                    list_of_files[file_hash][name] = [file_path]
             else:
-                duplicates[file_hash] = {name: [file_path]}
-    return duplicates
+                list_of_files[file_hash] = {name: [file_path]}
+    return list_of_files
 
 
 def print_duplicates(duplicates_dictionary):
@@ -38,5 +38,5 @@ def print_duplicates(duplicates_dictionary):
 
 if __name__ == '__main__':
     folder_path = parser_arg()
-    duplicates = find_duplicate(folder_path)
-    print_duplicates(duplicates)
+    dictionary_of_duplicates = find_duplicate(folder_path)
+    print_duplicates(dictionary_of_duplicates)
